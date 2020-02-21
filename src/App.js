922c87109d8c2764d97cf62a0fd6a01fb0163005
Brian Moreno <contact@bmediax.com>
2020-02-21 14:52:00 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Page Components
+import Welcome from './components/Welcome'
+import About from './components/About'
+import Contact from './components/Contact'
+import Navigation from './navigation/Navigation'
+
+import { Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <Route exact path="/" render={(...props) => <Welcome name="Brian" />} />
+      <Route path="/About" component={About} />
+      <Route path="/Contact" component={Contact} />
     </div>
   );
 }
