6bf8e513fb743d4d6a15d2935a4522318b7db254
Brian Moreno <contact@bmediax.com>
2020-02-21 17:06:56 -0800

    // Compiled Avenue View

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { Route } from 'react-router-dom'
 
 // Page Components
 import Welcome from './components/Welcome'
@@ -7,18 +8,19 @@ import About from './components/About'
 import Contact from './components/Contact'
 import Navigation from './navigation/Navigation'
 
-import { Route } from 'react-router-dom'
-
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Route exact path="/" render={(props) => <Welcome {...props} name="Brian" />} />
-      <Route path="/welcome/:name" component={Welcome} />
-      <Route path="/About" component={About} />
-      <Route path="/Contact" component={Contact} />
-    </div>
-  );
+// Application Function
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Route exact path="/" render={(props) => <Welcome {...props} name="Brian" />} />
+        <Route path="/welcome/:name" component={Welcome} />
+        <Route path="/About" component={About} />
+        <Route path="/Contact" component={Contact} />
+      </div>
+    );
+  }
 }
 
 export default App;
